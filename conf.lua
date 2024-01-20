7622561b78a09dbac3cf476fb0adfdabfc02315e
Ryan Cuff <rjcuff2000@gmail.com>
2024-01-20 15:12:12 -0500

    first commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.window.width = 1920
+    t.window.height = 1080
+    t.window.title = "Zomebie Shooter"
+    t.window.highdpi = true
+    t.window.resizable = true
+end
+
+
+
